ead05a2ec6e36620669098e5b96e118178e5a8ff
Jed Mao <jedmao@outlook.com>
2015-11-17 08:54:12 -0600

    Test nested blocks

---
+++ test/fixtures/nested-blocks/expected.js
@@ -0,0 +1,29 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+
+exports.default = function () {
+	return React.createElement(
+		"div",
+		{
+			className: "b1"
+		},
+		React.createElement(
+			"div",
+			{
+				className: "b1__e1"
+			},
+			React.createElement(
+				"div",
+				{
+					className: "b2"
+				},
+				React.createElement("div", {
+					className: "b2__e2"
+				})
+			)
+		)
+	);
+};
---
+++ test/fixtures/nested-blocks/input.jsx
@@ -0,0 +1,9 @@
+export default () => (
+	<div block="b1">
+		<div element="e1">
+			<div block="b2">
+				<div element="e2" />
+			</div>
+		</div>
+	</div>
+);
